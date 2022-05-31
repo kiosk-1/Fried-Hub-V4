dca71f38beb7f8b50aa548a2ab22c7ffae566364
Ray <105155637+kiosk-1@users.noreply.github.com>
2022-05-31 17:26:28 +0100

    Update Fried Hub V4.lua

--- Fried Hub V4.lua
+++ Fried Hub V4.lua
@@ -65,28 +65,6 @@
             loadstring(game:HttpGet('print("LOL! FLUNG!!!")'))()
             end)
 
-            -- GODMODE
-            local GodMode = Window:NewTab("God Mode")
-            local GodModeSection = GodMode:NewSection("GodMode")
-            GodModeSection:NewDropdown("GodMode", "This allows you to have god mode enabled", {"GodMode"}, function(v)
-                addcmd('god',{},function(args, speaker)
-                    local Cam = workspace.CurrentCamera
-                    local Pos, Char = Cam.CFrame, speaker.Character
-                    local Human = Char and Char.FindFirstChildWhichIsA(Char, "Humanoid")
-                    local nHuman = Human.Clone(Human)
-                    nHuman.Parent, speaker.Character = Char, nil
-                    nHuman.SetStateEnabled(nHuman, 15, false)
-                    nHuman.SetStateEnabled(nHuman, 1, false)
-                    nHuman.SetStateEnabled(nHuman, 0, false)
-                    nHuman.BreakJointsOnDeath, Human = true, Human.Destroy(Human)
-                    speaker.Character, Cam.CameraSubject, Cam.CFrame = Char, nHuman, wait() and Pos
-                    nHuman.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
-                    local Script = Char.FindFirstChild(Char, "Animate")
-                    if Script then
-                        Script.Disabled = true
-                        wait()
-                        Script.Disabled = false
-                    end
-                    nHuman.Health = nHuman.MaxHealth
-                end)()
-            end)
+            -- Credits
+            local GodMode = Window:NewTab("Credits")
+            local GodModeSection = GodMode:NewSection("Credits to Rayz")
